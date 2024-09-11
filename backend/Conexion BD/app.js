d6d977ceb9eecd4f345996c1efce55c1c3939dba
unknown <yersdani.2019@gmail.com>
2024-09-10 20:25:49 -0500

    Realización de pruebas getUsuarioID en el servidor conectado a la base de datos-Pruebas de Conexion

--- backend/Conexion BD/app.js
+++ backend/Conexion BD/app.js
@@ -1,15 +1,21 @@
-import express from "express";
+import express, { json } from "express";
 import{
-
+    getObjetivoByID,
+    getPerfilLoginByID,
+    getPuntoInteresByID,
+    getRutaByID,
+    getUsuarioByID
 }from "./database.js";
 
 const app= express();
 app.use(express.json());
 
-app.get("/todos/id", async(req,res)=>{
-    const
-})
+app.get("/usuario/:id", async(req,res)=>{
+    const usuario = await getUsuarioByID(req.params.id);
+    res.status(200).send(usuario);
+});
 
 app.listen(3000,()=>{
     console.log("Server running on port 3000")
 });
+ 
--- backend/Conexion BD/database.js
+++ backend/Conexion BD/database.js
@@ -2,16 +2,36 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql  //conexion con nuestra base de datos
-.createPool({
-    host: process.env.MYSQL_HOST, 
-    user: process.env.MYSQL_USER, 
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-})
-.promise();
+  })
+  .promise();
 
-export async function getTodoByID(id){
-    const [row]= await pool.query('SELECT * FROM todos WHERE id= ?'[id] )
+export async function getUsuarioByID(id) {
+    const [rows] = await pool.query('SELECT * FROM usuario WHERE id = ?',[id]);  // Aquí es donde corregimos el paso de parámetros
+    return rows[0];
+}
+
+export async function getObjetivoByID(id) {
+    const [row]= await pool.query('SELECT * FROM objetivo WHERE id= ?',[id] )
+    return row[0];
+}
+
+export async function getRutaByID(id) {
+    const [row]= await pool.query('SELECT * FROM ruta WHERE id= ?',[id] )
+    return row[0];
+}
+
+export async function getPuntoInteresByID(id) {
+    const [row]= await pool.query('SELECT * FROM punto_interes WHERE id= ?',[id] )
+    return row[0];
+}
+
+export async function getPerfilLoginByID(id) {
+    const [row]= await pool.query('SELECT * FROM perfil_Login WHERE id= ?',[id] )
     return row[0];
 }
