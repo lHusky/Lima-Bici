7d1b4f93165eb723068e79c238af8cdf5c30813b
unknown <villalv.diana7368@gmail.com>
2024-09-12 23:24:19 -0500

    ruta CrearUsuario

--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -1,3 +1,10 @@
 
 
 // CREAR USUARIO - POST
+const express = require('express');
+
+const { crearUsuario } = require('../controllers/controladores');
+const router = express.Router();
+
+router.post('/usuarios', crearUsuario);
+module.exports = router;
