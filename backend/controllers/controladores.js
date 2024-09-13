581bf25f6349695fe7ee0fbfb87536650f72500c
unknown <villalv.diana7368@gmail.com>
2024-09-12 23:25:28 -0500

    correccion ruta CrearUsuario

--- backend/controllers/controladores.js
+++ backend/controllers/controladores.js
@@ -1,12 +1,15 @@
 
-// CLASE USUARIO - Crear Usuario
+// CLASE PERFIL_LOGIN - Crear Usuario
 const express = require("../modules/clases");
 
 
 const crearUsuario = (req,res) => {
     const {nombre, email, contrasena,telefono} = req.body;
     const nuevoUsuario = Usurio.crearUsuario({nombre, email, contrasena,telefono})
+    
     res.status(201).json(nuevoUsuario);
 }
 
 module.exports= {crearUsuario};
+
+
