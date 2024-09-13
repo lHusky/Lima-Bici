667fb68afe88ade95bcad0c07ade445d4c8344f1
unknown <villalv.diana7368@gmail.com>
2024-09-12 23:14:28 -0500

    controlador CrearUsurio de Clase Usuario

--- backend/controllers/controladores.js
+++ backend/controllers/controladores.js
@@ -1,2 +1,10 @@
 
-// CREAR USUARIO - POST
+// CLASE USUARIO - Crear Usuario
+const express = require("../modules/clases.js")
+
+
+const createUsuario = (req,res) => {
+    const {nombre, email, contrasena,telefono} = req.body;
+    const nuevoUsuario = Usurio.createUsuario({nombre, email, contrasena,telefono})
+    res.status(201).json(nuevoUsuario);
+}
