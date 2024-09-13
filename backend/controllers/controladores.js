5ab851d0064a98a9bf5ac43b8f286a16a09f5412
unknown <villalv.diana7368@gmail.com>
2024-09-12 22:53:43 -0500

    archivos controladore, rutas

---
+++ backend/controllers/controladores.js
@@ -0,0 +1,2 @@
+
+// CREAR USUARIO - POST
---
+++ backend/modules/clases.js
@@ -0,0 +1,5 @@
+//Utilidad:
+    // Se crearán todas las clases aquí 
+//Averiguar:
+    // es dificil crear clases de node js?
+    // como definir las relaciones entre clases en node js?
