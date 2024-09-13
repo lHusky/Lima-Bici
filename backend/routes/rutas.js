61bf809061246898f9f624f800fc771b0affac54
unknown <yersdani.2019@gmail.com>
2024-09-12 23:18:28 -0500

    agregar ruta crear usuario

--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -1,10 +1,8 @@
-
-
-// CREAR USUARIO - POST
 const express = require('express');
-
 const { crearUsuario } = require('../controllers/controladores');
 const router = express.Router();
+
+// CREAR USUARIO - POST
 
 router.post('/usuarios', crearUsuario);
 module.exports = router;
