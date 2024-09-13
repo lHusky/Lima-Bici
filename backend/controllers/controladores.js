8532db7acddb007d69a9a0e59d26d0c3dc5b24ee
unknown <villalv.diana7368@gmail.com>
2024-09-12 23:19:52 -0500

    correccion controlador CrearUsuario

--- backend/controllers/controladores.js
+++ backend/controllers/controladores.js
@@ -1,10 +1,12 @@
 
 // CLASE USUARIO - Crear Usuario
-const express = require("../modules/clases.js")
+const express = require("../modules/clases");
 
 
-const createUsuario = (req,res) => {
+const crearUsuario = (req,res) => {
     const {nombre, email, contrasena,telefono} = req.body;
-    const nuevoUsuario = Usurio.createUsuario({nombre, email, contrasena,telefono})
+    const nuevoUsuario = Usurio.crearUsuario({nombre, email, contrasena,telefono})
     res.status(201).json(nuevoUsuario);
 }
+
+module.exports= {crearUsuario};
