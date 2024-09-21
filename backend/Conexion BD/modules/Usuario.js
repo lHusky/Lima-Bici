1b35ba034567b712ce92e0e5019d6c120752073f
sandrarmg <130387193+sandrarmg@users.noreply.github.com>
2024-09-20 22:19:59 -0500

    actualizacion clase usuario

--- backend/Conexion BD/modules/Usuario.js
+++ backend/Conexion BD/modules/Usuario.js
@@ -2,21 +2,25 @@
 import { getUsuarioByID, createUsuario } from '../database.js';
 
 class Usuario {
-    constructor(id, nombre, email, telefono, fechaCumple, fotoPerfil) {
+    constructor(id, nombre, email, telefono, fechaCumple, fotoPerfil, contrasena, peso, estadoSesion, estadoRecorrido) {
         this.id = id;
         this.nombre = nombre;
         this.email = email;
         this.telefono = telefono;
         this.fechaCumple = fechaCumple;
         this.fotoPerfil = fotoPerfil;
+        this.contrasena = contrasena;
+        this.peso = peso;
+        this.estadoSesion = estadoSesion;
+        this.estadoRecorrido = estadoRecorrido;
     }
 
     // Métodos Usuario
-    static async crearUsuario(nombre, email, telefono) {
+    static async crearUsuario(nombre, email, telefono, contrasena) {
         try {
             const [result] = await pool.query(
-                'INSERT INTO usuario (nombre, email, telefono) VALUES (?, ?, ?)',
-                [nombre, email, telefono]
+                'INSERT INTO usuario (nombre, email, telefono, contrasena) VALUES (?, ?, ?, ?)',
+                [nombre, email, telefono, contrasena]
             );
             const usuarioID = result.insertId;
             return { id: usuarioID };
@@ -25,6 +29,66 @@ class Usuario {
             throw error;
         }
     }
+
+    // Usuario activo
+    static async usuarioActivo(){
+        this.estadoSesion = true;
+    }
+
+    // Usuario inactivo
+    static async usuarioInactivo(){
+        this.estadoSesion = false;
+    }
+
+    // Getters y Setters  
+    setPesoUsuario(peso) {
+        this.peso = peso;
+    }
+
+    getPesoUsuario(){
+        return this.peso;
+    }
+
+    getNombre() {
+        return this.nombre;
+    }
+
+    setNombre(nombre) {
+        this.nombre = nombre;
+    }
+
+    getTelefono() {
+        return this.telefono;
+    }
+
+    setTelefono(telefono) {
+        this.telefono = telefono;
+    }
+
+    getContrasena() {
+        return this.contrasena;
+    }
+
+    setContrasena(contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    getFotoPerfil() {
+        return this.fotoPerfil;
+    }
+
+    setFotoPerfil(fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    getFechaCumple() {
+        return this.fechaCumple;
+    }
+
+    setFechaCumple(fechaCumple) {
+        this.fechaCumple = fechaCumple;
+    }
+
 }
 
 export default Usuario;
