5741fae70eac6df7a2bf30d21f12ca96f55b451e
francot2 <105623223+francot2@users.noreply.github.com>
2024-09-22 17:06:28 -0500

    Update PaginaPrincipal.jsx

--- frontend/limabici-app/screens/PaginaPrincipal/PaginaPrincipal.jsx
+++ frontend/limabici-app/screens/PaginaPrincipal/PaginaPrincipal.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import { View, Alert } from 'react-native';
-import Carrousel from '../../components/CarrouselFavoritos/Carrousel.jsx';
+import Footer from '../../components/footer/footer.jsx';
 
 const PaginaPrincipal = ({ navigation }) => {
-    const data = [
-        { id: 1, nombre: 'Mapa 1', imagen: 'https://example.com/map1.jpg' },
-        { id: 2, nombre: 'Mapa 2', imagen: 'https://example.com/map2.jpg' },
-        // Agrega más mapas aquí
-    ];
-
-    const handleCardPress = (id) => {
-        Alert.alert('Card Pressed', `You pressed card with ID: ${id}`);
-        // Aquí puedes navegar a la pantalla de detalle si lo deseas
-        // navigation.navigate('DetalleProducto', { itemId: id });
-    };
-
     return (
         <View>
-            <Carrousel data={data} onCardPress={handleCardPress} />
+            <Footer />
         </View>
     );
 };
