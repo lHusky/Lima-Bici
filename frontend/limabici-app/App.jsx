44aae47d0c25d1a38311a68228bf9b6a984eaf70
francot2 <105623223+francot2@users.noreply.github.com>
2024-09-22 16:58:23 -0500

    Update App.jsx

--- frontend/limabici-app/App.jsx
+++ frontend/limabici-app/App.jsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RegistroUsuario from './screens/RegistroUsuario/RegistroUsuario.jsx';
 import Iniciosesion from './screens/InicioSesion/InicioSesion.jsx';
+import PaginaPrincipal from './screens/PaginaPrincipal/PaginaPrincipal.jsx';
+
 
 const Stack = createStackNavigator();
 
@@ -12,6 +14,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Iniciosesion">
         <Stack.Screen name="Iniciosesion" component={Iniciosesion} />
         <Stack.Screen name="RegistroUsuario" component={RegistroUsuario} />
+        <Stack.Screen name="PaginaPrincipal" component={PaginaPrincipal} />
       </Stack.Navigator>
     </NavigationContainer>
   );
