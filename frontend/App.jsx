86f28212c72537ad0e8f5709745801c607ef346c
francot2 <105623223+francot2@users.noreply.github.com>
2024-09-23 01:18:52 -0500

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,41 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RegistroUsuario from './screens/RegistroUsuario/RegistroUsuario.jsx';
 import Iniciosesion from './screens/InicioSesion/InicioSesion.jsx';
-import gestionUsuarioApi from './api/gestionUsuario.js';
+import PaginaInicio from './screens/PaginaInicio/PaginaInicio.jsx';
+import PaginaFavoritos from './screens/PaginaFavoritos/PaginaFavoritos.jsx';
+
+import Footer from './components/footer/footer.jsx';
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [userAccounts, setUserAccounts] = useState([]);
-
-    useEffect(() => {
-        const cargaDeDatos = async () => {
-            try {
-                const response = await gestionUsuarioApi.findAll();
-                
-                const usuariosData = response.usuarios; // Accede a la propiedad 'usuarios'
-            
-                setUserAccounts(usuariosData); 
-            } catch (error) {
-                console.error('Error al cargar usuarios:', error);
-            }
-        };
-        cargaDeDatos();
-    }, []);
-// PROP: es un componente inmutable que se declara en app (padre) y se puede utilizar en cada pestaña (hijo)
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Iniciosesion">
-        <Stack.Screen name="Iniciosesion">
-            {(props) => <Iniciosesion {...props} userAccounts={userAccounts} />} 
-            
-        </Stack.Screen>
-        <Stack.Screen name="RegistroUsuario">
-            {(props) => <RegistroUsuario {...props} userAccounts={userAccounts} />}
-        </Stack.Screen>
-        
+      <Stack.Navigator initialRouteName="Footer">
+      <Stack.Screen name="Footer" component={Footer} />
+        <Stack.Screen name="Iniciosesion" component={Iniciosesion} />
+        <Stack.Screen name="RegistroUsuario" component={RegistroUsuario} />
+        <Stack.Screen name="PaginaInicio" component={PaginaInicio} />
+        <Stack.Screen name="PaginaFavoritos" component={PaginaFavoritos} />
       </Stack.Navigator>
     </NavigationContainer>
   );
