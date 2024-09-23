2fd9c2b111c30ef9a04ea2f2f32b9c6093b0e5f4
Diana Villalva <151112725+lHusky@users.noreply.github.com>
2024-09-23 15:14:28 -0500

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RegistroUsuario from './screens/RegistroUsuario/RegistroUsuario.jsx';
@@ -12,12 +12,33 @@ import Footer from './components/footer/footer.jsx';
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [userAccounts, setUserAccounts] = useState([]);
+    useEffect(() => {
+        const cargaDeDatos = async () => {
+            try {
+                const response = await gestionUsuarioApi.findAll();
+                
+                const usuariosData = response.usuarios; // Accede a la propiedad 'usuarios'
+            
+                setUserAccounts(usuariosData); 
+            } catch (error) {
+                console.error('Error al cargar usuarios:', error);
+            }
+        };
+        cargaDeDatos();
+    }, []);
+// PROP: es un componente inmutable que se declara en app (padre) y se puede utilizar en cada pestaña (hijo)
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Footer">
       <Stack.Screen name="Footer" component={Footer} />
-        <Stack.Screen name="Iniciosesion" component={Iniciosesion} />
-        <Stack.Screen name="RegistroUsuario" component={RegistroUsuario} />
+        <Stack.Screen name="Iniciosesion">
+            {(props) => <Iniciosesion {...props} userAccounts={userAccounts} />} 
+        </Stack.Screen>
+         <Stack.Screen name="RegistroUsuario">
+            {(props) => <RegistroUsuario {...props} userAccounts={userAccounts} />}
+        </Stack.Screen>
         <Stack.Screen name="PaginaInicio" component={PaginaInicio} />
         <Stack.Screen name="PaginaFavoritos" component={PaginaFavoritos} />
       </Stack.Navigator>
