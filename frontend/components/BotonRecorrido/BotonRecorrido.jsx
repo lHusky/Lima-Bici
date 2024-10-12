9a5ebb0fe26846ac8ff4537b8307f122187ccf0c
francot2 <105623223+francot2@users.noreply.github.com>
2024-10-11 21:58:26 -0500

    Update BotonRecorrido.jsx

--- frontend/components/BotonRecorrido/BotonRecorrido.jsx
+++ frontend/components/BotonRecorrido/BotonRecorrido.jsx
@@ -1,47 +1,44 @@
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-const BotonRecorrido = () => {
-  const handleStartPress = () => {
-    // Aquí puedes manejar la acción cuando se presione el botón
-    console.log("Botón Iniciar presionado");
-  };
+const BotonIniciar = ({ onPress, tracking }) => {
+    useEffect(() => {
+        console.log('BotonIniciar: el estado de tracking ha cambiado a:', tracking);
+    }, [tracking]);
 
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.startButton} onPress={handleStartPress}>
-        <Text style={styles.buttonText}>Iniciar</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    const buttonStyle = tracking ? styles.botonDetener : styles.botonIniciar;
+
+    return (
+        <TouchableOpacity style={buttonStyle} onPress={onPress}>
+            <Text style={styles.texto}>{tracking ? 'Detener' : 'Iniciar'}</Text>
+        </TouchableOpacity>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute', // Posiciona de manera absoluta
-    bottom: '25%', // Centrado en la mitad de la pantalla verticalmente
-    left: '50%', // Centrado horizontalmente
-    transform: [{ translateX: -40 }, { translateY: 40 }], // Ajuste para el centrado del botón (depende de su tamaño)
-    zIndex: 1, // Asegura que esté por encima del mapa
-  },
-  startButton: {
-    backgroundColor: 'orange',
-    width: 80,
-    height: 80,
-    borderRadius: 40, // Hace que el botón sea circular
-    justifyContent: 'center', // Centra el texto dentro del botón
-    alignItems: 'center',
-    elevation: 5, // Sombra en Android
-    shadowColor: '#000', // Sombra en iOS
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+    botonIniciar: {
+        backgroundColor: '#6ECF68', // Verde para "Iniciar"
+        padding: 15,
+        borderRadius: 100,
+        flex: 1,
+        marginRight: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    botonDetener: {
+        backgroundColor: '#FF0000', // Rojo para "Detener"
+        padding: 15,
+        borderRadius: 100,
+        flex: 1,
+        marginRight: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    texto: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 });
 
-export default BotonRecorrido;
+export default BotonIniciar;
