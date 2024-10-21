71cab0f9e759e67d5d50e420cd788db2ac43cb79
francot2 <105623223+francot2@users.noreply.github.com>
2024-10-21 11:27:36 -0500

    Update PaginaFavoritos.jsx

--- frontend/screens/PaginaFavoritos/PaginaFavoritos.jsx
+++ frontend/screens/PaginaFavoritos/PaginaFavoritos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import HeaderFavoritos from '../../components/header/headerFavoritos.jsx'
+import Header from '../../components/header/header.jsx';
 import Footer from '../../components/footer/footer.jsx';
 
 import RutasFavoritas from '../../components/RutasFavoritas/RutasFavoritas.jsx';
@@ -9,7 +9,7 @@ import PuntosDeInteres from '../../components/PuntosDeInteres/PuntosDeInteres.js
 const PaginaFavoritos = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <HeaderFavoritos />
+            <Header title="Favoritos" />
             <View style={styles.content}>
                 <RutasFavoritas />
                 <PuntosDeInteres />
