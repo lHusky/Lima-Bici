4aad0006325b03f21b0e0c1c841e84ca5e15fc9d
sandrarmg <130387193+sandrarmg@users.noreply.github.com>
2024-11-13 23:23:16 -0500

    mapa main

--- frontend/components/Mapa/Mapa.jsx
+++ frontend/components/Mapa/Mapa.jsx
@@ -1,72 +1,66 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, Modal, ActivityIndicator, TouchableOpacity, Linking, Image, ScrollView, Text } from 'react-native';
-import MapView, { Marker, Polyline } from 'react-native-maps';
-import MapViewDirections from 'react-native-maps-directions';
-import { Card, Paragraph, Button } from 'react-native-paper';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, StyleSheet, Dimensions, Alert } from 'react-native';
+import { useGooglePlaces } from '../../context/ContextAPI/GooglePlacesContext';
+import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
-import 'react-native-get-random-values';
-import uuid from 'react-native-uuid';
-import FormPuntoInteres from './FormPuntoInteres/FormPuntoInteres.jsx'
+
+import MapMarker from './Marker';
+import FixedRoutes from './FixedRoutes';
+import BlueRoute from './BlueRoute';
+import YellowRoute from './YellowRoute';
+import RedRoute from './RedRoute';
+import RoutePlanner from '../../utils/RoutePlanner';
+import PlaceDetailsModal from './PlaceDetailsModal';
+
+import useUserLocation from '../../hooks/useUserLocation';
+import useLocationTracker from '../../hooks/useLocationTracker';
 
 const { width, height } = Dimensions.get('window');
 
-const Mapa = ({ apiKey }) => {
+const Mapa = ({ destination, setDestination, trackUser, apiKey, onMarkerDragEnd }) => {
     const mapRef = useRef(null);
-    const [origin, setOrigin] = useState(null);
-    const [destination, setDestination] = useState(null);
-    const [routeCoordinates, setRouteCoordinates] = useState([]);
-    const [selectedLocation, setSelectedLocation] = useState(null);
+    const { location: origin, error } = useUserLocation();
+    const { routeCoordinates, startTracking, stopTracking, distance, duration } = useLocationTracker();
+    const [initialOrigin, setInitialOrigin] = useState(null);
+    const [isMapReady, setIsMapReady] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
     const [placeDetails, setPlaceDetails] = useState(null);
+    const [selectedLocation, setSelectedLocation] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [showFormPuntoInteres, setShowFormPuntoInteres] = useState(false);
 
-    useEffect(() => {
-        const getCurrentLocation = async () => {
-            const { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                alert('Permiso de localización denegado');
-                return;
-            }
-            const location = await Location.getCurrentPositionAsync({});
-            setOrigin({
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-            });
-        };
+    const { fetchRouteDetails, blueRouteCoords } = useGooglePlaces();
+    const [adjustedRouteCoords, setAdjustedRouteCoords] = useState([]);
+    const [bikePaths, setBikePaths] = useState([]);
 
-        getCurrentLocation();
-    }, []);
-
-    const fetchPlaceDetails = async (latitude, longitude) => {
+    const handleLongPress = async (event) => {
+        const { latitude, longitude } = event.nativeEvent.coordinate;
+        setSelectedLocation({ latitude, longitude });
+        setModalVisible(true);
         setLoading(true);
         try {
             const responseGeocode = await fetch(
                 `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`
             );
             const geocodeResult = await responseGeocode.json();
-
             if (geocodeResult.results && geocodeResult.results.length > 0) {
                 const placeId = geocodeResult.results[0].place_id;
-
                 const responseDetails = await fetch(
                     `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${apiKey}`
                 );
                 const detailsResult = await responseDetails.json();
-
                 if (detailsResult.result) {
                     const place = detailsResult.result;
                     setPlaceDetails({
-                        name: place.name,
-                        address: place.formatted_address,
-                        phoneNumber: place.formatted_phone_number || 'No disponible',
-                        rating: place.rating || 'No disponible',
-                        types: place.types ? place.types.join(', ') : 'No disponible',
+                        name: place.name || "Ubicación Seleccionada",
+                        address: place.formatted_address || "No disponible",
+                        phoneNumber: place.formatted_phone_number || "No disponible",
+                        rating: place.rating || "No disponible",
+                        types: place.types ? place.types.join(", ") : "No disponible",
                         photos: place.photos ? place.photos.map(photo => `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=${apiKey}`) : [],
                     });
-                } else {
-                    setPlaceDetails({ name: "Lugar desconocido", address: "No se encontró información" });
                 }
+            } else {
+                setPlaceDetails({ name: "Ubicación Seleccionada", address: "No disponible" });
             }
         } catch (error) {
             console.error("Error al obtener detalles del lugar:", error);
@@ -76,38 +70,63 @@ const Mapa = ({ apiKey }) => {
         }
     };
 
-    const handleLongPress = (event) => {
-        const { latitude, longitude } = event.nativeEvent.coordinate;
-        setSelectedLocation({ latitude, longitude });
-        setModalVisible(true);
-        fetchPlaceDetails(latitude, longitude);
-    };
-
     const closeModal = () => {
         setModalVisible(false);
         setSelectedLocation(null);
         setPlaceDetails(null);
     };
 
-    const cancelRoute = () => {
-        setDestination(null);
-        setRouteCoordinates([]);
-    };
-
-
-    const handleGuardarPunto = () => {
-        setModalVisible(false);
-        // setShowFormPuntoInteres(false);
-        setTimeout(() => setShowFormPuntoInteres(true), 300); // Volver a abrir el segundo modal
-    };
-    
+    useEffect(() => {
+        if (error) {
+            Alert.alert('Error', error);
+        }
+        if (origin && !initialOrigin) {
+            setInitialOrigin(origin);
+            setDestination(origin);
+        }
+    }, [origin, error, initialOrigin]);
 
     useEffect(() => {
-        console.log("Estado del modal principal:", modalVisible);
-        console.log("Estado del formulario de punto de interés:", showFormPuntoInteres);
-    }, [modalVisible, showFormPuntoInteres]);
-    
-    
+        if (trackUser) {
+            startTracking((newCoordinate) => {
+                if (mapRef.current) {
+                    mapRef.current.animateCamera({ center: newCoordinate });
+                }
+            });
+        } else {
+            stopTracking();
+            if (routeCoordinates.length > 0) {
+                saveTrackedRoute();
+            }
+        }
+        return () => {
+            stopTracking();
+        };
+    }, [trackUser]);
+
+    const saveTrackedRoute = async () => {
+        if (initialOrigin && destination && distance && duration) {
+            const routeData = {
+                userId: 1,
+                nombre: "Ruta recorrida",
+                descripcion: "Ruta desde el origen hasta el destino.",
+                distancia: distance,
+                duracion: duration,
+                fechaInicio: new Date().toISOString(),
+                fechaFin: new Date(new Date().getTime() + duration * 60).toISOString(),
+                coordenadas: routeCoordinates.map(coord => ({
+                    lat: coord.latitude,
+                    lng: coord.longitude,
+                })),
+            };
+            try {
+                await api.create(routeData);
+                Alert.alert('Ruta Guardada', 'La ruta recorrida ha sido guardada exitosamente.');
+            } catch (error) {
+                Alert.alert('Error', 'No se pudo guardar la ruta recorrida.');
+            }
+        }
+    };
 
     return (
         <View style={styles.container}>
@@ -115,147 +134,47 @@ const Mapa = ({ apiKey }) => {
                 ref={mapRef}
                 style={styles.map}
                 initialRegion={{
-                    latitude: origin ? origin.latitude : -12.0464,
-                    longitude: origin ? origin.longitude : -77.0428,
-                    latitudeDelta: 0.05,
-                    longitudeDelta: 0.05,
+                    latitude: initialOrigin ? initialOrigin.latitude : -12.046374,
+                    longitude: initialOrigin ? initialOrigin.longitude : -77.042793,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005,
                 }}
+                onMapReady={() => setIsMapReady(true)}
                 onLongPress={handleLongPress}
             >
-                {origin && <Marker coordinate={origin} title="Tu ubicación" />}
-
+                {initialOrigin && <MapMarker coordinate={initialOrigin} />}
                 {destination && (
-                    <Marker
+                    <MapMarker
                         coordinate={destination}
-                        title="Destino"
-                        pinColor="blue"
-                    />
-                )}
-
-                {origin && destination && (
-                    <MapViewDirections
-                        origin={origin}
-                        destination={destination}
-                        apikey={apiKey}
-                        strokeWidth={6}
-                        strokeColor="blue"
-                        mode="WALKING"
-                        onReady={(result) => {
-                            setRouteCoordinates(result.coordinates);
-                            mapRef.current.fitToCoordinates(result.coordinates, {
-                                edgePadding: {
-                                    right: 50,
-                                    bottom: 50,
-                                    left: 50,
-                                    top: 50,
-                                },
-                            });
-                        }}
-                        onError={(errorMessage) => {
-                            console.error("Error al obtener la ruta:", errorMessage);
+                        draggable
+                        onDragEnd={(e) => {
+                            const { latitude, longitude } = e.nativeEvent.coordinate;
+                            if (onMarkerDragEnd) {
+                                onMarkerDragEnd(latitude, longitude);
+                            }
                         }}
                     />
                 )}
-
-                <Polyline coordinates={routeCoordinates} strokeWidth={5} strokeColor="red" />
-
-                {selectedLocation && (
-                    <Marker
-                        coordinate={selectedLocation}
-                        title="Ubicación seleccionada"
-                    />
-                )}
+                {blueRouteCoords && <BlueRoute coordinates={blueRouteCoords} />}
+                {adjustedRouteCoords && <YellowRoute coordinates={adjustedRouteCoords} />}
+                <RoutePlanner route={blueRouteCoords} bikePaths={bikePaths} onRouteCalculated={setAdjustedRouteCoords} />
+                <FixedRoutes onRoutesLoaded={(data) => setBikePaths(data)} />
+                <RedRoute coordinates={routeCoordinates} />
             </MapView>
 
-            {destination && (
-                <TouchableOpacity style={styles.cancelButton} onPress={cancelRoute}>
-                    <Text style={styles.cancelButtonText}>Cancelar ruta</Text>
-                </TouchableOpacity>
-            )}
-
-            <Modal visible={modalVisible} animationType="slide" transparent={true}>
-                <View style={styles.modalContainer}>
-                    <View style={styles.modalContent}>
-                        <ScrollView contentContainerStyle={styles.scrollViewContent}>
-                            <Card style={styles.card}>
-                                <Card.Title title={placeDetails?.name || "Ubicación Seleccionada"} />
-                                {loading ? (
-                                    <ActivityIndicator animating={true} color="#0000ff" />
-                                ) : (
-                                    <>
-                                        <Card.Content>
-                                            <Paragraph>Dirección: {placeDetails?.address || "No disponible"}</Paragraph>
-                                            <Paragraph>Teléfono: {placeDetails?.phoneNumber || "No disponible"}</Paragraph>
-                                            <Paragraph>Calificación: {placeDetails?.rating} ⭐</Paragraph>
-                                            <Paragraph>Tipos: {placeDetails?.types || "No disponible"}</Paragraph>
-                                        </Card.Content>
-                                        {placeDetails?.photos && placeDetails.photos.length > 0 && (
-                                            <Image
-                                                source={{ uri: placeDetails.photos[0] }}
-                                                style={{ width: 100, height: 100, marginVertical: 10 }}
-                                            />
-                                        )}
-                                        <ScrollView horizontal contentContainerStyle={styles.buttonContainer} showsHorizontalScrollIndicator={false}>
-                                            <Button
-                                                mode="contained"
-                                                onPress={() => {
-                                                    setDestination({
-                                                        latitude: selectedLocation.latitude,
-                                                        longitude: selectedLocation.longitude,
-                                                    });
-                                                    closeModal();
-                                                }}
-                                                style={[styles.button, { backgroundColor: 'green' }]}
-                                            >
-                                                <Text style={styles.buttonText}>Indicaciones</Text>
-                                            </Button>
-                                            <Button
-                                                mode="contained"
-                                                onPress={() =>
-                                                    Linking.openURL(`https://www.google.com/maps/search/?api=1&query=${selectedLocation.latitude},${selectedLocation.longitude}`)
-                                                }
-                                                style={[styles.button, { backgroundColor: 'green' }]}
-                                            >
-                                                <Text style={styles.buttonText}>Ir a Google Maps</Text>
-                                            </Button>
-                                            <Button mode="contained" style={[styles.button, { backgroundColor: 'green' }]}>
-                                                <Text style={styles.buttonText}>Agregar a Favoritos</Text>
-                                            </Button>
-                                            <Button mode="contained" onPress={handleGuardarPunto} style={[styles.button, { backgroundColor: 'green' }]}>
-                                                <Text style={styles.buttonText}>Guardar Punto</Text>
-                                            </Button>
-                                            <Button mode="outlined" onPress={closeModal} style={[styles.button, { borderColor: 'green' }]}>
-                                                <Text style={[styles.buttonText, { color: 'green' }]}>Cerrar</Text>
-                                            </Button>
-                                        </ScrollView>
-                                    </>
-                                )}
-                            </Card>
-                        </ScrollView>
-                    </View>
-                </View>
-            </Modal>
-            {showFormPuntoInteres && (
-            <Modal visible={showFormPuntoInteres} transparent={true} animationType="slide" onRequestClose={() => setShowFormPuntoInteres(false)}>
-                <View style={styles.formContainer}>
-                    <FormPuntoInteres />
-                    <TouchableOpacity style={styles.cancelButton} onPress={() => setShowFormPuntoInteres(false)}>
-                         <Text style={styles.cancelButtonText}>Cerrar</Text>
-                     </TouchableOpacity>
-                </View>
-            </Modal>
-)}
-
+            <PlaceDetailsModal
+                visible={modalVisible}
+                placeDetails={placeDetails}
+                loading={loading}
+                onClose={closeModal}
+                setDestination={setDestination}
+                selectedLocation={selectedLocation}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    formContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    },
     container: {
         flex: 1,
     },
@@ -263,53 +182,10 @@ const styles = StyleSheet.create({
         width: width,
         height: height,
     },
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        backgroundColor: 'rgba(0,0,0,0.5)',
-    },
-    modalContent: {
-        maxHeight: '60%', 
-        backgroundColor: 'white',
-        paddingVertical: 20,
-        paddingHorizontal: 10,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-    },
-    scrollViewContent: {
-        flexGrow: 1,
-    },
-    card: {
-        backgroundColor: 'white',
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingTop: 10,
-    },
-    button: {
-        marginHorizontal: 5,
-        minWidth: 100,
-    },
-    buttonText: {
-        color: 'white',
-    },
-    cancelButton: {
-        position: 'absolute',
-        top: 92, 
-        right: 20,
-        backgroundColor: 'red',
-        paddingVertical: 8,
-        paddingHorizontal: 15,
-        borderRadius: 20,
-    },
-    cancelButtonText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
 });
 
 export default Mapa;
+
 
 
 
